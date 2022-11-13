900a23922a3237306f90dfccf02f9cde51f7609c
Will Ripley <willdripley@gmail.com>
2022-11-13 14:07:50 -0600

    Changed data selection to 5%

--- data_selection.sh
+++ data_selection.sh
@@ -1,3 +1,3 @@
 python data_selection.py --task_name mnli --model_name electra-small-discriminator \
-  --proportion .25 --burn_out 3 --training_dynamics_prefix /content/drive/MyDrive/trainingCarto/mnli/electra/ \
+  --proportion .5 --burn_out 3 --training_dynamics_prefix /content/drive/MyDrive/trainingCarto/mnli/electra/ \
   --output_prefix /content/drive/MyDrive/trainingCarto/mnli/electra/quarterPercent
