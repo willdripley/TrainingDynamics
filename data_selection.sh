4073743517c00c9daaa31d41ff1718c1d2edbd0f
Will Ripley <willdripley@gmail.com>
2022-11-14 17:05:46 -0600

    fixed output prefix for 33 percent data selection

--- data_selection.sh
+++ data_selection.sh
@@ -1,3 +1,3 @@
 python data_selection.py --task_name mnli --model_name electra-small-discriminator \
   --proportion .33 --burn_out 3 --training_dynamics_prefix /content/drive/MyDrive/trainingCarto/mnli/electra/ \
-  --output_prefix /content/drive/MyDrive/trainingCarto/mnli/electra/thirtyPercent
+  --output_prefix /content/drive/MyDrive/trainingCarto/mnli/electra/thirtythreePercent
